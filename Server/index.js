b2e48c0c7a9356710abaaa116b4ee518ce599fbc
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2025-12-12 12:57:58 +0000

    added server setup with app listening on specified port

---
+++ Server/index.js
@@ -0,0 +1,8 @@
+require('dotenv').config()
+
+const app = require("./app")
+const port = process.env.port
+
+app.listen(port, () => {
+  console.log(`Hackathon---Bridget-Jones-Diary is running on ${port}`)
+})

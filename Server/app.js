20b82029a22361d7aa3b4c1fda71af1438560942
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2025-12-12 13:12:30 +0000

    added root route to serve API information

--- Server/app.js
+++ Server/app.js
@@ -7,6 +7,14 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get("/", (req, res) => {
+  console.log("running index from app.js");
+  res.status(200).json({
+    title: "Bridget Jones Diary API",
+    description: "Welcome to the Bridget Jones Diary API!",
+  })
+})
+
 app.use("/entries", entryRouter)
 
 export default app

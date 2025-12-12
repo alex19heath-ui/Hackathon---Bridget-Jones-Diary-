16da8da09b633a7e0f0f5c59a9d1e212a55da8c6
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2025-12-12 15:06:31 +0000

    changed  connection setup to use Pool from pg for improved connection management

--- Server/database/connect.js
+++ Server/database/connect.js
@@ -1,15 +1,19 @@
 import * as schema from "./schema.js"
 import { drizzle } from "drizzle-orm/node-postgres"
+import { Pool } from "pg"
+import dotenv from "dotenv"
 
-const db = drizzle({
-  schema,
-  connection: {
-    password: process.env.DB_PASSWORD,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-  },
+dotenv.config()
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 })
+
+const db = drizzle(pool, { schema })
 
 console.log("DB connection established.")
 

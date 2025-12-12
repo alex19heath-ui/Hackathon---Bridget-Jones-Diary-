a90348532ec17ffc6d32c93839e80b763b154b37
Yasmin Reina Hamouda <118626907+yasminrei@users.noreply.github.com>
2025-12-12 11:44:34 +0000

    created connection that hooks up credentials to db const. also hook up schema file to the db variable so you can access database information from this variable

---
+++ Server/database/connect.js
@@ -0,0 +1,12 @@
+import * as schema from "./schema"
+import { drizzle } from "drizzle-orm/node-postgres"
+
+export const db = drizzle({
+    schema,
+    connection: {
+        password: process.env.DB_PASSWORD,
+        user: process.env.DB_USER,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+    }
+})
